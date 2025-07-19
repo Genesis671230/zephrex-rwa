be95b34899b03945753c75266fc4547d68c9399c
Hamzeph <hamid.h@mobiusdtaas.ai>
2025-07-20 02:28:42 +0400

    auto deployment

--- src/pages/BackOffice/BackOffice.tsx
+++ src/pages/BackOffice/BackOffice.tsx
@@ -266,7 +266,7 @@ const BackOffice: React.FC = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-6 space-y-6">
+    <div className="min-h-screen pt-[5%] bg-gradient-to-br from-slate-50 to-slate-100">
       <ToastContainer />
       
       {/* Header */}
